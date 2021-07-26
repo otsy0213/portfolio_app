e2fcf2d9a0b4521b879220b6bb1e033fea7e77eb
shoya <otsy0213@gmail.com>
2021-07-27 06:49:01 +0900

    devise不要のため削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   get 'members/show', to: 'members#show'
   resources :apos
-  devise_for :companies, controllers: { registrations: 'companies/registrations',
-                                        sessions: 'companies/sessions',
-                                        passwords: 'companies/passwords'}
-  devise_for :members, controllers: { registrations: 'members/registrations',
-                                      sessions: 'members/sessions',
-                                      passwords: 'members/passwords'}
 end
